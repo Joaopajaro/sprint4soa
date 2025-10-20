c34eb173efeebf77933beae8f43dea8414362b20
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-10-20 16:06:46 -0300

    Update JwtResponse.java

--- SoaApp/SoaApp/src/main/java/com/example/soaauthapp/dto/JwtResponse.java
+++ SoaApp/SoaApp/src/main/java/com/example/soaauthapp/dto/JwtResponse.java
@@ -1,10 +1,6 @@
 package com.example.soaauthapp.dto;
 
-/**
- * DTO retornado após autenticação bem-sucedida. Contém apenas o
- * token JWT gerado pelo serviço, mas em aplicações reais poderia
- * incluir também informações sobre o usuário e roles.
- */
+
 public class JwtResponse {
     private String token;
 
