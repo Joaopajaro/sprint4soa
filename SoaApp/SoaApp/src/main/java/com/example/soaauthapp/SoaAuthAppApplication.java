81353853a70f9d610b5d619fa23e5c6ca533aa3f
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-10-20 16:08:44 -0300

    Update SoaAuthAppApplication.java

--- SoaApp/SoaApp/src/main/java/com/example/soaauthapp/SoaAuthAppApplication.java
+++ SoaApp/SoaApp/src/main/java/com/example/soaauthapp/SoaAuthAppApplication.java
@@ -3,14 +3,7 @@ package com.example.soaauthapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Classe principal da aplicação SOA de autenticação. Esta classe contém o
- * método main que inicia o contexto Spring. O objetivo da aplicação é
- * demonstrar conceitos de arquitetura orientada a serviços com
- * autenticação/autorizações seguras usando JWT. O código foi escrito
- * de forma simples e comentada para fins didáticos, como seria
- * desenvolvido por um estudante.
- */
+
 @SpringBootApplication
 public class SoaAuthAppApplication {
 
