5b547b47dc242c9e916df0dde2a701dc60bd40f8
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-10-20 16:07:44 -0300

    Update TaskRepository.java

--- SoaApp/SoaApp/src/main/java/com/example/soaauthapp/repository/TaskRepository.java
+++ SoaApp/SoaApp/src/main/java/com/example/soaauthapp/repository/TaskRepository.java
@@ -4,10 +4,7 @@ import com.example.soaauthapp.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repositório de tarefas. Estende JpaRepository para operações CRUD
- * automáticas. Este repositório será injetado no serviço TaskService.
- */
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
